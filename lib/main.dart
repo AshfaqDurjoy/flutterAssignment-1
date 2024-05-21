9461fb2ff1a3f5ffe782dacf9c185dfa295e5b1f
AshfaqDurjoy <137197948+AshfaqDurjoy@users.noreply.github.com>
2024-05-22 01:04:34 +0600

    Added Assests
    added assets and pubspec.yaml changed

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,30 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget{
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      debugShowCheckedModeBanner: False,
+      home: new MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+
+    _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+    );
+  }
+
+}
+
