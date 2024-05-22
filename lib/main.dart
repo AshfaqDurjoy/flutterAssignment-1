b12685f0bdfd6b541cce2d86b9d91c5c169d8ca4
AshfaqDurjoy <137197948+AshfaqDurjoy@users.noreply.github.com>
2024-05-22 19:12:33 +0600

    Completed Login page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +27,7 @@ class _MyHomePageState extends State<MyHomePage>{
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children:<Widget> [
@@ -52,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage>{
                     child: Text(
                         '.',
                         style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold,color: Colors.green)
+                            fontSize: 80.0, fontWeight: FontWeight.bold,color: Colors.blue)
                     )
                 )
               ],
@@ -71,13 +74,128 @@ class _MyHomePageState extends State<MyHomePage>{
                       color: Colors.grey,
                     ),
                     focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)
+                      borderSide: BorderSide(color: Colors.blue)
                     )
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                TextField(
+                  decoration: InputDecoration(
+                   labelText:'PASSWORD',
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                ),
+                focusedBorder: UnderlineInputBorder(
+                 borderSide: BorderSide(color: Colors.green)
+                )
+                ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  alignment: Alignment(1.0,0.0),
+                  padding: EdgeInsets.only(top: 15.0,left: 20.0),
+                  child: InkWell(
+                    child: Text('Forgot Password',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                          decoration:TextDecoration.underline),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.greenAccent,
+                    color:Colors.blue,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){
+
+                      },
+                      child: Center(
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  height: 40.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 1.0,
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Center(
+                          child: ImageIcon(AssetImage('assets/facebook.png')),
+                        ),
+                        SizedBox(width: 10.0),
+                        Center(
+                          child: Text('Log in using Facebook',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold
+                          ),
+                          ),
+                        )
+
+                      ],
+                    ),
                   ),
                 )
               ],
             ),
+          ),
+          SizedBox(height: 15.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+              'Don\'t have an account ?',
+              style: TextStyle(
+                fontFamily: 'Montserrat'
+              ),
+          ),
+          SizedBox(width: 5.0),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Text('Register',
+            style: TextStyle(
+              color: Colors.blue,
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline
+            ),
+            ),
           )
+            ],
+          )
+
         ],
       ),
     );
